bea257067f1728a6097208f2276999115a83a772
JC-Ge <gxl200896@163.com>
2019-05-20 22:37:13 +0800

    add avatar

--- router/api/users.js
+++ router/api/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/User')
 const bcrypt = require('bcrypt')
+const gravatar = require('gravatar')
 
 router.get('/test',(req,res)=>{
     res.json({msg:'login works'})
@@ -13,9 +14,11 @@ router.post('/register',(req,res)=>{
         if(user){
             return res.status(400).json({email:"邮箱已经存在！"})
         }else{
+            var avatar = gravatar.url(req.body.email, {s: '200', r: 'pg', d: 'mm'});  // s:size r:rating
             const newUser =new User({
                 name:req.body.name,
                 email:req.body.email,
+                avatar,
                 password:req.body.password,
             })
             bcrypt.genSalt(10,(err,salt) => {
