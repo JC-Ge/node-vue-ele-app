b072335d43396b86875dfd87333616addb066f45
JC-Ge <gxl200896@163.com>
2019-05-20 22:05:02 +0800

    update

--- router/api/users.js
+++ router/api/users.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
+const bcrypt = require('bcrypt')
 
 router.get('/test',(req,res)=>{
     res.json({msg:'login works'})
+})
+
+router.post('/register',(req,res)=>{
+    User.findOne({email:req.body.email})
+    .then(user=>{
+        if(user){
+            return res.status(400).json({email:"邮箱已经存在！"})
+        }else{
+            const newUser =new User({
+                name:req.body.name,
+                email:req.body.email,
+                password:req.body.password,
+            })
+            bcrypt.genSalt(10,(err,salt) => {
+                bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err))
+                })
+            })
+        }
+    })
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,20 +3,24 @@ const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
 const app = new express()
 const users = require('./router/api/users')
-
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 5000
+
+
+
+app.use(bodyParser.urlencoded({extended:false}))  // 在新的node版本中，bodyParser的配置要写在路由配置之前
+app.use(bodyParser.json())
+
 app.use('/api/users',users)
 
-app.get('/',(req,res)=>{
-    mongoose.connect(db,{ useNewUrlParser: true })
-    .then(()=>{
-        console.log("connected mongodb")
-        res.send('hello')
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+mongoose.connect(db,{ useNewUrlParser: true })
+.then(()=>{
+    console.log("connected mongodb")
+})
+.catch((err)=>{
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!未连接成功')
+    console.log(err)
 })
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
