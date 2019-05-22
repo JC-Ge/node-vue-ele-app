ac0cd1f5997c7a2d4fb00d7fe1bd878a6230ac38
JC-Ge <gxl200896@163.com>
2019-05-22 18:30:11 +0800

    update

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({extended:false}))  // 在新的node版本中，bodyParser的配置要写在路由配置之前
 app.use(bodyParser.json())
 
-
 app.use('/api/users',users)
 app.use('/api/profiles',profiles)
 
