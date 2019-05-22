ff3ee00ef076439383938bae430fcb64f7cf6ecc
JC-Ge <gxl200896@163.com>
2019-05-22 22:16:58 +0800

    edit content of register

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from './views/Index'
+import Register from './views/Register'
+import NotFound from './views/404'
 
 Vue.use(Router)
 
@@ -13,9 +15,19 @@ export default new Router({
       redirect:"/index"
     },
     {
-      path:'/',
+      path:'/index',
       name:"index",
       component:Index
+    },
+    {
+      path:'/register',
+      name:"register",
+      component:Register
+    },
+    {
+      path:'*',
+      name:'/404',
+      component:NotFound
     }
   ]
 })
